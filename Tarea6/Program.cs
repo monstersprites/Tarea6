449e1357c5196322c781a35c0ae1d9368ec9f0f7
monstersprites <49540368+monstersprites@users.noreply.github.com>
2021-03-02 16:53:44 -0400

    Add files via upload

---
+++ Tarea6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tarea6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tarea 6!");
+            Console.WriteLine("Hola Mundo!");
+        }
+    }
+}
