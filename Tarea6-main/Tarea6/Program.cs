edaa81bf62513b8dbc9bdbf387a13aae03a36e1d
ronaldramonambroisesanchez <67200559+ronaldramonambroisesanchez@users.noreply.github.com>
2021-03-02 17:11:32 -0400

    Modifficando Primeros Archivos

---
+++ Tarea6-main/Tarea6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tarea6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tarea 6!");
+            Console.WriteLine("Hola Mundo!");
+            COnsole.WriteLine("Como estan?");
+        }
+    }
+}
